8b6a192b6a6e04cf02a587bdaa8e57175816c829
krismurillo22 <kristian_murillo@unitec.edu>
2024-09-20 13:04:27 -0600

    Subida

---
+++ src/examen2_lab2/Examen2_Lab2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2_lab2;
+
+/**
+ *
+ * @author User
+ */
+public class Examen2_Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
